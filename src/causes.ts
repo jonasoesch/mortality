7059e34768c6188af889d1c7bf8f738eab64a82c
Jonas Oesch <jonas.oesch@fhnw.ch>
2019-01-25 18:20:39 +0100

    Removed superflous morphing graph

--- src/causes.ts
+++ src/causes.ts
@@ -7,17 +7,6 @@ Promise.all([uptickGraph(), aidsGraph()]).then(graphs => {
 
     const d = new Director()
 
-    const highlightMiddleAge = graphs[0].cloneWithNewName("highlight")
-    highlightMiddleAge.removeMark("Rate85up")
-    highlightMiddleAge.removeMark("Rate75_84")
-    highlightMiddleAge.removeMark("Rate65_74")
-    highlightMiddleAge.removeMark("Rate55_64")
-    highlightMiddleAge.removeMark("Rate45_54")
-    highlightMiddleAge.removeMark("Rate_25")
-    d.addStep(1800, 2000, highlightMiddleAge) 
-
-
-
     // Storyboard
     d.addStep(-200, 50, graphs[0])
     d.addStep(-200, 40, scrollIndicator)
