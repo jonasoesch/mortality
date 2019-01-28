a97df73120787f6bc1af0afd19d388c127ba6526
Jonas Oesch <jonas.oesch@fhnw.ch>
2019-01-28 18:55:07 +0100

    Static versions

--- src/causes.ts
+++ src/causes.ts
@@ -4,30 +4,6 @@ import {MorphingGraphWithLabels, MorphingGraph} from './MorphingGraph'
 import {scrollIndicator} from './scrollIndicator'
 
 Promise.all([uptickGraph(), aidsGraph()]).then(graphs => {
-
-    const d = new Director()
-
-    const morph = new MorphingGraph("uptick-aids")
-    morph.setOrigin(graphs[0]) // uptick
-    morph.setTarget(graphs[1]) // aids
-    morph.addTransition("Rate25_44", "Other")
-    morph.addTransition("Rate25_44", "Cancer")
-    morph.addTransition("Rate25_44", "Heart")
-    morph.addTransition("Rate25_44", "LiverKidneyPancreas")
-    morph.addTransition("Rate25_44", "Suicide")
-    morph.addTransition("Rate25_44", "Vehicle")
-    morph.addTransition("Rate25_44", "Homicide")
-    morph.addTransition("Rate25_44", "Respiratory")
-    morph.addTransition("Rate25_44", "Drug_induced")
-    morph.addTransition("Rate25_44", "AIDS")
-
-
-    // Storyboard
-    d.addStep(-200, 50, graphs[0])
-    d.addStep(-200, 40, scrollIndicator)
-    d.addStep(200, 500, morph)
-    d.addStep(500, 10000, graphs[1])
-
     graphs[0].draw()
     graphs[1].draw()
 })
--- src/relative.ts
+++ src/relative.ts
@@ -4,25 +4,6 @@ import {MorphingGraphWithLabels} from './MorphingGraph'
 import {scrollIndicator} from './scrollIndicator'
 
 Promise.all([ageDifferencesGraph(), uptickGraph()]).then(graphs => {
-    const d = new Director()
-
-
-    let morph = new MorphingGraphWithLabels("differences-uptick")
-    morph.setOrigin(graphs[0]) // differences
-    morph.setTarget(graphs[1]) // uptick
-    morph.addTransition("Rate_25"  , "Rate_25"  ).setLabel("Under 25")
-    morph.addTransition("Rate25_44", "Rate25_44").setLabel("25–44")
-    morph.addTransition("Rate45_54", "Rate45_54").setLabel("45–54")
-    morph.addTransition("Rate55_64", "Rate55_64").setLabel("55–64")
-    morph.addTransition("Rate65_74", "Rate65_74").setLabel("65-74")
-    morph.addTransition("Rate75_84", "Rate75_84").setLabel("75–84")
-    morph.addTransition("Rate85up" , "Rate85up" ).setLabel("Over 84")
-
-    d.addStep(-200, 50, graphs[0])
-    d.addStep(-200, 40, scrollIndicator)
-    d.addStep(50, 500, morph)
-    d.addStep(500, 10000, graphs[1])
-
     graphs[0].draw()
     graphs[1].draw()
 })
